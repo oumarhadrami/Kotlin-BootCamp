fb50526454d1e037c855d357f6cc64053eb396c4
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-05-06 01:01:51 +0530

    Special Classes

---
+++ Classes/SpecialClasses.kt
@@ -0,0 +1,12 @@
+object Konosuba{
+    val author = "Teruhashi"
+    fun drawing(){ 
+        println("Currently drawing")
+    }
+}
+
+enum class AnimeGenre (val studio: String){
+    Fantasy(studio = "DavidPRoductions"),
+    Shonen( studio = "TOEI")
+}
+
